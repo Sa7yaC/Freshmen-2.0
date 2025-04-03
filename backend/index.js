f546ac3397eda6f1f8a778eda419388b11a0ef66
sa7ya <131731688+Sa7yaC@users.noreply.github.com>
2025-04-03 15:45:48 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 
 const app = express()
 const port = 3000
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
